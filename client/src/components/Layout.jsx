089044b0660aa8b26d992d0d2d6aabca401e6fb6
kamma-vamshi <k.vamshi0009@gmail.com>
2026-04-07 21:39:47 +0530

    Update Layout.jsx

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,6 +1,6 @@
-// src/components/Layout.jsx
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
+import BottomNav from "./BottomNav";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import socket from "../services/socket";
@@ -60,21 +60,24 @@ const Layout = ({ children }) => {
   }, [userInfo, navigate]);
 
   return (
-    <div className="flex h-screen bg-gray-950 text-white">
+    <div className="flex h-screen bg-gray-950 text-white overflow-hidden">
       
       {/* SIDEBAR */}
       <Sidebar />
 
       {/* RIGHT SIDE */}
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col relative overflow-hidden">
         
         {/* NAVBAR */}
         <Navbar />
 
         {/* CONTENT */}
-        <main className="flex-1 overflow-y-auto p-6 pt-24">
+        <main className="flex-1 overflow-y-auto p-4 md:p-6 pt-20 md:pt-24 pb-28 lg:pb-6 custom-scrollbar">
           {children}
         </main>
+
+        {/* MOBILE BOTTOM NAV */}
+        <BottomNav />
 
       </div>
     </div>
